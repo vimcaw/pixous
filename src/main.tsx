e7ed1868ea514034e2917d55ad8c3a801b571f85
vimcaw <vimcaw@gmail.com>
2022-07-02 01:33:32 +0800

    chore(deps): upgrade all dependents

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider as SpectrumProvider, darkTheme } from '@adobe/react-spectrum';
 import './i18n';
 import './index.scss';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+if (!container) throw new Error('No container found');
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <SpectrumProvider theme={darkTheme}>
       <App />
     </SpectrumProvider>
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );

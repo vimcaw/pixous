4706a628e007c81f06669ede0fb7cfb985893ccf
vimcaw <vimcaw@gmail.com>
2022-07-24 14:39:21 +0800

    chore: don't auto open dev server

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { ViteAliases } from 'vite-aliases';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), ViteAliases()],
-  server: {
-    open: true,
-  },
   esbuild: {
     jsxInject: "import React from 'react';",
   },
